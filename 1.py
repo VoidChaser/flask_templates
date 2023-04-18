beec325321e5d3a2c03227216f15eb8d4372559a
trofi <troxa75@yandex.ru>
2023-04-18 23:41:41 +0300

    Third_task

--- 1.py
+++ 1.py
@@ -16,13 +16,19 @@ def index():
 
 prophs = {
     'инженер-исследователь': 'inj',
-    'инженер-строитель': 'inj',
     'пилот': 'sci',
-    'метеоролог': 'sci',
-    'инженер по жизнеобеспечению': 'sci',
-    'инеженер по радиационной защите': 'inj',
+    'строитель': 'inj',
+    'экзобиолог': 'sci',
     'врач': 'sci',
-    'экзобиолог': 'sci'
+    'инженер по терраформированию': 'sci',
+    'астрогеолог': 'sci',
+    'гляциолог': 'sci',
+    'инженер жизнеобеспеченея': 'inj',
+    'метеоролог': 'sci',
+    'оператор марсохода': 'inj',
+    'киберинженер': 'sci',
+    'штурман': 'sci',
+    'пилот дронов': 'sci',
 }
 
 @app.route('/training/<proph>')
@@ -44,6 +50,12 @@ def proph(proph):
         param['image_path'] = url_for('static', filename='img/science_s.png')
 
     return render_template('prophecy_inj.html', **param)
+
+
+@app.route('/list_prof/<list_mark>')
+def list_p(list_mark):
+    return render_template('list_prof.html', mark=list_mark, proph_list=list(prophs.keys()))
+
 
 
 if __name__ == '__main__':
