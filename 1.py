d21adc5654d7b87bed984abd49343eaf39dd38ff
trofi <troxa75@yandex.ru>
2023-04-19 16:12:11 +0300

    Fourth_task

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, url_for, request
 import json
 
 
@@ -9,8 +9,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/')
 @app.route('/index')
 def index():
-    param = {}
-    param['Заготовка'] = 'Домашняя страница'
+    param = {'Заготовка': 'Домашняя страница'}
     return render_template('base.html', **param)
 
 
@@ -31,15 +30,16 @@ prophs = {
     'пилот дронов': 'sci',
 }
 
-@app.route('/training/<proph>')
-def proph(proph):
-    param = {}
-    param['proph_name_form'] = proph
-    prophecy_name = proph.lower()
-    if (prophecy_name in prophs.keys() and prophs[prophecy_name] == 'inj') or ('строитель' in prophecy_name or 'инженер' in prophecy_name):
+
+@app.route('/training/<prophec>')
+def proph(prophec):
+    param = {'proph_name_form': prophec}
+    prophecy_name = prophec.lower()
+    if (prophecy_name in prophs.keys() and prophs[prophecy_name] == 'inj')\
+            or ('строитель' in prophecy_name or 'инженер' in prophecy_name):
         param['Заготовка'] = 'Маршрут для инженеров'
         param['font_color'] = 'green'
-        param['dep_name'] = 'инженерные тренажеры'
+        param['dep_name'] = 'Инженерные тренажеры'
         param['font_size_int'] = 12
         param['image_path'] = url_for('static', filename='img/inj_t.png')
     elif prophecy_name not in prophs.keys() or prophs[prophecy_name] == 'sci':
@@ -56,6 +56,33 @@ def proph(proph):
 def list_p(list_mark):
     return render_template('list_prof.html', mark=list_mark, proph_list=list(prophs.keys()))
 
+
+@app.route('/answer', methods=['POST', 'GET'])
+def form_sample():
+    if request.method == 'GET':
+        return render_template('form_sample.html')
+
+    elif request.method == 'POST':
+        parameters = {'Фамилия': request.form['surname'], 'Имя': request.form['name'],
+                      'Образование': request.form['class']}
+
+        prophecys = []
+        prophecys.append('Инженер-исследователь') if request.form.get('accept1') else ''
+        prophecys.append('Инженер-строитель') if request.form.get('accept2') else ''
+        prophecys.append('Пилот') if request.form.get('accept3') else ''
+        prophecys.append('Метеоролог') if request.form.get('accept4') else ''
+        prophecys.append('Инженер по жизнеобеспечению') if request.form.get('accept5') else ''
+        prophecys.append('Инженер по радиационной защите') if request.form.get('accept6') else ''
+        prophecys.append('Врач') if request.form.get('accept7') else ''
+        prophecys.append('Экзобиолог') if request.form.get('accept8') else ''
+
+        parameters['Профессия'] = \
+            f"{', '.join(list(filter(lambda x: x != '', prophecys))) if prophecys else 'не указано'}"
+        parameters['Пол'] = request.form.get("sex")
+        parameters['О себе'] = request.form.get("about")
+        parameters['Готов остаться'] = 'Готов' if request.form.get("acceptRules") else 'Не готов'
+        print(parameters)
+        return render_template('auto_answer.html', parameters=parameters)
 
 
 if __name__ == '__main__':
